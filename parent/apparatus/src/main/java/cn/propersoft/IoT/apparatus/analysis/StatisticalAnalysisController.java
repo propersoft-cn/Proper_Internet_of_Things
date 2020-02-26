5f1b43d9deafd1f275f7586cc81d4c416f615748
zhanglei <605860283@qq.com>
2020-02-26 16:13:34 +0800

    增加统计分析数据接口电量 水量统计分析柱状图接口

--- parent/apparatus/src/main/java/cn/propersoft/IoT/apparatus/analysis/StatisticalAnalysisController.java
+++ parent/apparatus/src/main/java/cn/propersoft/IoT/apparatus/analysis/StatisticalAnalysisController.java
@@ -1,6 +1,9 @@
 package cn.propersoft.IoT.apparatus.analysis;
 
 import cn.hutool.core.convert.Convert;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.RandomUtil;
 import cn.propersoft.IoT.apparatus.chapai.entity.ChaPaiEntity;
 import cn.propersoft.IoT.apparatus.chapai.service.ChaPaiService;
 import cn.propersoft.IoT.response.ResultBody;
@@ -9,7 +12,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
+import java.util.*;
 
 @RestController
 @Api(tags = "统计分析接口")
@@ -20,27 +23,78 @@ public class StatisticalAnalysisController {
     private ChaPaiService chaPaiService;
 
     @ResponseBody
-    @GetMapping("/getFloorBatteryData/{floor}/battery")
-    @ApiOperation(value = "各楼层能耗分析")
-    public ResultBody getFloorBatteryData(@PathVariable String floor, String startTime, String endTime) {
+    @GetMapping("/getFloorBatteryData/{floor}/pie")
+    @ApiOperation(value = "各楼层能耗分析饼图")
+    public ResultBody getFloorBatteryPieData(@PathVariable String floor, String startTime, String endTime) {
         ChaPaiEntity chaPaiEntity = chaPaiService.getFloorData(floor, new Date(Convert.toLong(startTime)), new Date(Convert.toLong(endTime)));
         return ResultBody.success(chaPaiEntity);
     }
 
+    @ResponseBody
+    @GetMapping("/getFloorBatteryData/{floor}/histogram")
+    @ApiOperation(value = "各楼层能耗分析柱状图")
+    public ResultBody getFloorBatteryHistogramData(@PathVariable String floor, String startTime, String endTime) {
+        List<Map> result = new ArrayList<Map>();
+        DateTime dateTime = DateUtil.date(new Date());
+        Calendar calendar = dateTime.toCalendar();
+        for (int i = 0; i < 6; i++) {
+            Map map = new HashMap();
+            calendar.add(Calendar.MONTH, i);
+            String format = DateUtil.format(calendar.getTime(), "yyyy-MM");
+            map.put(format, RandomUtil.randomInt(100, 150));
+            result.add(map);
+        }
+        return ResultBody.success(result);
+    }
+
 
     @ResponseBody
-    @GetMapping("/getDeviceBatteryData/{deviceId}/battery")
-    @ApiOperation(value = "各设备能耗分析")
+    @GetMapping("/getDeviceBatteryData/{deviceId}/pie")
+    @ApiOperation(value = "各设备能耗分析饼图")
     public ResultBody getDeviceBatteryData(@PathVariable String deviceId, String startTime, String endTime) {
         ChaPaiEntity chaPaiEntity = chaPaiService.getFloorData(deviceId, new Date(Convert.toLong(startTime)), new Date(Convert.toLong(endTime)));
         return ResultBody.success(chaPaiEntity);
     }
 
     @ResponseBody
-    @GetMapping("/getFloorWaterData/{floor}/water")
-    @ApiOperation(value = "各楼层用水分析")
+    @GetMapping("/getDeviceBatteryData/{deviceId}/histogram")
+    @ApiOperation(value = "各设备能耗分析柱状图")
+    public ResultBody getDeviceBatteryHistogramData(@PathVariable String deviceId) {
+        List<Map> result = new ArrayList<Map>();
+        DateTime dateTime = DateUtil.date(new Date());
+        Calendar calendar = dateTime.toCalendar();
+        for (int i = 0; i < 6; i++) {
+            Map map = new HashMap();
+            calendar.add(Calendar.MONTH, i);
+            String format = DateUtil.format(calendar.getTime(), "yyyy-MM");
+            map.put(format, RandomUtil.randomInt(100, 150));
+            result.add(map);
+        }
+        return ResultBody.success(result);
+    }
+
+    @ResponseBody
+    @GetMapping("/getFloorWaterData/{floor}/pie")
+    @ApiOperation(value = "各楼层用水分析饼图")
     public ResultBody getFloorWaterData(@PathVariable String floor, String startTime, String endTime) {
         ChaPaiEntity chaPaiEntity = chaPaiService.getFloorData(floor, new Date(Convert.toLong(startTime)), new Date(Convert.toLong(endTime)));
         return ResultBody.success(chaPaiEntity);
+    }
+
+    @ResponseBody
+    @GetMapping("/getFloorWaterData/{floor}/histogram")
+    @ApiOperation(value = "各楼层用水分析柱状图")
+    public ResultBody getFloorWaterHistogramData(@PathVariable String floor) {
+        List<Map> result = new ArrayList<Map>();
+        DateTime dateTime = DateUtil.date(new Date());
+        Calendar calendar = dateTime.toCalendar();
+        for (int i = 0; i < 6; i++) {
+            Map map = new HashMap();
+            calendar.add(Calendar.MONTH, i);
+            String format = DateUtil.format(calendar.getTime(), "yyyy-MM");
+            map.put(format, RandomUtil.randomInt(100, 150));
+            result.add(map);
+        }
+        return ResultBody.success(result);
     }
 }
---
+++ parent/core/src/main/java/cn/propersoft/IoT/filter/CorsConfig.java
@@ -0,0 +1,8 @@
+package cn.propersoft.IoT.filter;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class CorsConfig extends WebMvcConfigurerAdapter {
+
+}
