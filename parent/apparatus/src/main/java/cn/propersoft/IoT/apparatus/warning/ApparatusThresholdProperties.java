aebd29709493cd37511dce2448444a2181d05389
zhanglei <605860283@qq.com>
2020-03-07 11:19:16 +0800

    调整阈值默认值

--- parent/apparatus/src/main/java/cn/propersoft/IoT/apparatus/warning/ApparatusThresholdProperties.java
+++ parent/apparatus/src/main/java/cn/propersoft/IoT/apparatus/warning/ApparatusThresholdProperties.java
@@ -10,29 +10,29 @@ import org.springframework.stereotype.Component;
 public class ApparatusThresholdProperties {
 
     private Double dianYaMin = 0d;
-    private Double dianYaMax = 0d;
+    private Double dianYaMax = 20000d;
     private Double dianLiuMin = 0d;
-    private Double dianLiuMax = 0d;
+    private Double dianLiuMax = 20000d;
     /**
      * 插入式温度
      */
     private Double cWenDuMin = 0d;
-    private Double cWenDuMax = 0d;
+    private Double cWenDuMax = 20000d;
     private Double co2Min = 0d;
-    private Double co2Max = 0d;
+    private Double co2Max = 20000d;
     private Double shuiQinMin = 0d;
-    private Double shuiQinMax = 0d;
+    private Double shuiQinMax = 20000d;
     /**
      * 温度湿度表的温度
      */
     private Double wsWenDuMin = 0d;
-    private Double wsWenDuMax = 0d;
+    private Double wsWenDuMax = 20000d;
     private Double wsShiDuMin = 0d;
-    private Double wsShiDuMax = 0d;
+    private Double wsShiDuMax = 20000d;
     private Double yaLiMin = 0d;
-    private Double yaLiMax = 0d;
+    private Double yaLiMax = 20000d;
     private Double yeWeiMin = 0d;
-    private Double yeWeiMax = 0d;
+    private Double yeWeiMax = 20000d;
 
 
 }
--- parent/demo/src/main/java/cn/propersoft/IoT/demo/controller/DemoController.java
+++ parent/demo/src/main/java/cn/propersoft/IoT/demo/controller/DemoController.java
@@ -81,8 +81,97 @@ public class DemoController {
             LOGGER.info("MSG SEND SUCCESS");
         }
 
-//        WebSocketServer.sendInfo(message, toUserId);
         return ResponseEntity.ok("MSG SEND SUCCESS");
     }
+
+//    @ResponseBody
+//    @GetMapping("/quartzTest1")
+//    @ApiOperation(value = "定时任务调度测试")
+//    public ResultBody quartzTest1(String f) {
+//
+//        //TODO 根据元数据id获取元数据信息
+//        //TODO 根据目标数据id获取目标数据信息
+//        //TODO 获取数据源信息
+//        //TODO 启动定时任务 记录定时任务id
+//        Scheduler scheduler = CronUtil.getScheduler();
+//        if (!scheduler.isStarted()) {
+//            CronUtil.start();
+//        }
+//        CronUtil.setMatchSecond(true);
+//        String id = CronUtil.schedule("*/3 * * * * *", new Runnable() {
+//            @Override
+//            public void run() {
+//                Console.log("task running : 3s");
+//            }
+//        });
+//        //TODO 保存定时任务详情表中（taskid，元id，目id，备注）
+//        //TODO 建立连接获取数据,校验数据合法性
+//        //TODO 根据id获取映射配置
+//        //TODO 根据映射配置处理转存数据
+//        //TODO 记录数据抽取结果
+//
+//
+//        return ResultBody.success();
+//
+//    }
+//
+//    @ResponseBody
+//    @GetMapping("/quartzTest2")
+//    @ApiOperation(value = "定时任务调度测试")
+//    public ResultBody quartzTest2(String id) {
+//
+//        //TODO 根据元数据id获取元数据信息
+//        //TODO 根据目标数据id获取目标数据信息
+//        //TODO 获取数据源信息
+//        //TODO 启动定时任务 记录定时任务id
+//        //TODO 保存定时任务详情表中（taskid，元id，目id，备注）
+//        //TODO 建立连接获取数据,校验数据合法性
+//        //TODO 根据id获取映射配置
+//        //TODO 根据映射配置处理转存数据
+//        //TODO 记录数据抽取结果
+//        CronUtil.remove(id);
+//
+//        return ResultBody.success(id);
+//
+//    }
+//
+//
+//    @ResponseBody
+//    @GetMapping("/connDB")
+//    @ApiOperation(value = "连接数据源 ")
+//    public ResultBody connDB(String url, String username, String password) {
+//
+//        DataSource ds = new SimpleDataSource(url, username, password);
+//
+//        Connection conn = null;
+//        try {
+//            conn = ds.getConnection();
+//            // 执行非查询语句，返回影响的行数
+//            String sql = "CREATE TABLE `yali` (\n" +
+//                    "  `id` varchar(64) NOT NULL,\n" +
+//                    "  `add_time` datetime(6) NOT NULL,\n" +
+//                    "  `equipment_name` varchar(32) NOT NULL,\n" +
+//                    "  `yali_num` double DEFAULT NULL,\n" +
+//                    "  PRIMARY KEY (`id`)\n" +
+//                    ")";
+//            SqlExecutor.execute(conn, sql);
+////            int count = SqlExecutor.execute(conn, "UPDATE " + TABLE_NAME + " set field1 = ? where id = ?", 0, 0);
+////            LOGGER.info("影响行数：{}", count);
+//            // 执行非查询语句，返回自增的键，如果有多个自增键，只返回第一个
+////            Long generatedKey = SqlExecutor.executeForGeneratedKey(conn, "UPDATE " + TABLE_NAME + " set field1 = ? where id = ?", 0, 0);
+////            LOGGER.info("主键：{}", generatedKey);
+//
+//            /* 执行查询语句，返回实体列表，一个Entity对象表示一行的数据，Entity对象是一个继承自HashMap的对象，存储的key为字段名，value为字段值 */
+////            List<Entity> entityList = SqlExecutor.query(conn, "select * from " + TABLE_NAME + " where param1 = ?", new EntityListHandler(), "值");
+////            LOGGER.info("{}", entityList);
+//        } catch (SQLException e) {
+////            LOGGER.error(LOGGER, e, "SQL error!");
+//        } finally {
+//            DbUtil.close(conn);
+//        }
+//
+//        return null;
+//    }
+
 
 }
